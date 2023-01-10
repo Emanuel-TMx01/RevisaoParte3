c1ee8e9792b746ef9b4860a1684e03fcb35e465a
emanu <emanu@EMA_PC>
2023-01-10 19:27:50 +0000

    Add project files.

---
+++ Models/Author.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RevisaoParte3.Models
+{
+    public class Author
+    {
+        public int AuthorId { get; set; }
+
+        [StringLength(50, ErrorMessage ="Menos de 50 caracteres")]
+        [Required]
+        public string AuthorName { get; set; }
+
+    }
+}
